937d635a02829230affb091da2934dd13fd7d7d8
Jean Delarbre <delarbrejean@icloud.com>
2024-11-27 11:41:43 +0100

    débuté

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -1,2 +1,34 @@
 class OptionsController < ApplicationController
+  before_action set_event
+  def new
+    @option = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.date = Date.parse(params[:event][:date])
+    @event.charity = Charity.first
+    if @event.save!
+      redirect_to events_path
+    else
+      render "events/new",status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+  def set_event
+    @event = Event.find(params[:event_id])
+  end
+
+  def event_params
+    params.require(:event).permit(:name, :description, :date)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,7 @@
 
 User.destroy_all
 Charity.destroy_all
+Event.destoy_all
 
 User.create!(
   email: "jean@lewagon.org",
@@ -23,3 +24,18 @@ Charity.create!(
   phone_number: "+1234567890",
   # user_id: User.first.id
 )
+
+Event.create!(
+  name: "MAMA 2025",
+  description: "Passer un moment convivial de sport avec la marche ou la course, mais également de fête (musique, tombola et barbecue pour clôturer la journée).",
+  date: Date.new(2025, 10, 20),
+  charity_id: Charity.first
+)
+
+Event.create!(
+  name: "YUL 2024",
+  description: "Passer un moment convivial de sport avec la marche ou la course, mais également de fête (musique, tombola et barbecue pour clôturer la journée).",
+  date: Date.new(2024, 10, 05),
+  charity_id: Charity.first
+)
+
