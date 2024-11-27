d64c1ee6dc83e992e9c0cd473b9c594e1a6e8d59
Vincent Demaret <demaretvincent001@gmail.com>
2024-11-27 15:29:04 +0100

    creationTickets

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@ puts "------- creating Tickets ------------------"
   10.times do
     Ticket.create!(
       name: "Course 10km",
-      description: "Une course de 10 km",
-      unit_price: 12.0,
+      description: "sportifs c'est parti",
+      unit_price: 12,
       event_id:Event.last.id
     )
   end
