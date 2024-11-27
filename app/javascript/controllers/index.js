0f7acc475e6f669ea8871e38e74606b473c165da
Thomas Barbieux <thomas@fresh.win>
2024-11-27 16:06:35 +0100

    Payment integration OK - aucune connexion a Donation ou Participation pour l'instant

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,9 @@
-// Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+import { Application } from "@hotwired/stimulus";
+
+const application = Application.start();
+
+import StripeController from "./stripe_controller";
+
+application.register("stripe", StripeController);
+
+console.log("Stimulus controllers are being registered");
