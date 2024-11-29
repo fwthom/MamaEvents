6d2075a58eb6cc5e0b2e28636e6d0519b538c10b
Vincent Demaret <demaretvincent001@gmail.com>
2024-11-29 16:46:11 +0100

    cardEventController

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,9 @@
 class EventsController < ApplicationController
 skip_before_action :authenticate_user!
 before_action :set_events, only: [:index]
+before_action :set_charity, only: [:index, :show]
 
   def index
-
-    @charity = Charity.find(params[:charity_id])
     @events = Event.where(charity_id: @charity)
 
 
@@ -12,12 +11,15 @@ before_action :set_events, only: [:index]
 
   def show
     @event = Event.find(params[:id])
-    @charity = @event.charity
   end
 
   private
 
   def set_events
     @events = Event.all
+  end
+
+  def set_charity
+    @charity = Charity.first
   end
 end
