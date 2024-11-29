38e60b3d6a4f37388199e6208dcebf371771a1a1
Camille GUILMAIN <caguilmain@hotmail.fr>
2024-11-29 16:19:49 +0100

    ok chemin

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,8 @@ skip_before_action :authenticate_user!
 before_action :set_events, only: [:index]
 
   def index
+    @charity = Charity.find(params[:charity_id])
+    @events = @charity.events
   end
 
   def show
