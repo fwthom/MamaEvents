f6dbc77f443f03f905d6bcc4d53ff0725016bf33
Vincent Demaret <demaretvincent001@gmail.com>
2024-12-02 09:02:51 +0100

    Charities n'entre plus en compte quand on accède aux events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,20 +1,15 @@
 class EventsController < ApplicationController
 skip_before_action :authenticate_user!
-before_action :set_events, only: [:index]
+
 
   def index
-    @charity = Charity.find(params[:charity_id])
-    @events = @charity.events
+    @events = Event.all
   end
 
   def show
     @event = Event.find(params[:id])
-    @charity = @event.charity
+    @tickets = Ticket.where(event_id: @event.id)
   end
 
-  private
 
-  def set_events
-    @events = Event.all
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_29_150431) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_01_100206) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_29_150431) do
     t.index ["charity_id"], name: "index_events_on_charity_id"
   end
 
+  create_table "names", force: :cascade do |t|
+    t.string "description"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "options", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -87,6 +94,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_29_150431) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "event_id"
+    t.string "team"
     t.index ["team_id"], name: "index_participants_on_team_id"
   end
 
