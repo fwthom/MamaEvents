6c68fcca62b715a00ad2e397d509e1fb335e4096
Jean Delarbre <delarbrejean@icloud.com>
2024-12-03 09:38:28 +0100

    numéro de dossard ajouté

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_02_203953) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_03_083553) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -120,6 +120,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_02_203953) do
     t.datetime "updated_at", null: false
     t.integer "event_id"
     t.string "team"
+    t.integer "bib_number"
     t.index ["team_id"], name: "index_participants_on_team_id"
   end
 
