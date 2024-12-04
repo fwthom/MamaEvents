80849b4e021a5b34c69df38b9ad3e8ad15b9f669
Jean Delarbre <delarbrejean@icloud.com>
2024-12-04 17:46:31 +0100

    suppression top navbar

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -7,8 +7,7 @@ class Participant < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-
-
+  
   before_validation :generate_token, on: :create
 
   private
